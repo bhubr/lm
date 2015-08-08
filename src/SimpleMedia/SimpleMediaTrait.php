b411ba029bb40349d668885db841eef529ad93ac
Little Buddha <littlebuddha31@gmail.com>
2015-08-08 17:31:00 +0200

    Set image as default if no other is

--- src/SimpleMedia/SimpleMediaTrait.php
+++ src/SimpleMedia/SimpleMediaTrait.php
@@ -11,13 +11,16 @@ use Vidya\SimpleMedia\Models\Media;
 trait SimpleMediaTrait
 {
     function addMedia($name, $path) {
+        $defaults = $this->medias()->get()->pluck('default')->toArray();
+        // Add as default if no other is
+        $isDefault = array_search(1, $defaults) === false;
         $media = new Media([
             'object_id'   => $this->id,
             'object_type' => get_class($this),
             'object_slug' => $this->slug,
             'name' => $name,
             'path' => $path,
-            'default' => false
+            'default' => $isDefault
         ]);
         $this->medias()->save($media);
     }
